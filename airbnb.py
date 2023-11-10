16281eb0d012af9b174f52ae518063693efa7998
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-11-10 16:56:05 +0100

    update

--- airbnb.py
+++ airbnb.py
@@ -5,11 +5,15 @@ class MyConsole(cmd.Cmd):
     prompt = ">>> "
 
     def do_create(self, args):
-        print("this method create ", args)
+        print("this method create", args)
     
     def precmd(self, line):
-        command, other = line.split(" ")
-        line = f"{command} Shoes"
+        # command, other = line.split(" ")
+        # line = f"{command} Shoes"
+        if "." in line:
+           line = line.replace(".", " ").replace("(", "").replace(")", "")
+           line = line.split(" ")
+           line = f"{line[1]} {line[0]}"
         print(line)
         return cmd.Cmd.precmd(self, line)
 
