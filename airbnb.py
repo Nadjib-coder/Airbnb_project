2125156b434d7c5d31b2666c5e641d624efd25df
Nadjib-coder <nadjibmouhoun1@gmail.com>
2023-11-13 15:38:12 +0100

    update the cmd interpreter

--- airbnb.py
+++ airbnb.py
@@ -1,27 +1,28 @@
-#!/usr/bin/python3
-
 import cmd
-import sys
 
-class MyConsole(cmd.Cmd):
-    prompt = ">>> "
 
-    def do_create(self, args):
-        print("this method create", args)
-    
-    def precmd(self, line):
-        # command, other = line.split(" ")
-        # line = f"{command} Shoes"
-        # make the app work non-interactively
-        if not sys.stdin.isatty():
-            print()
-        if "." in line:
-           line = line.replace(".", " ").replace("(", "").replace(")", "")
-           line = line.split(" ")
-           line = f"{line[1]} {line[0]}"
-        print(line)
-        return cmd.Cmd.precmd(self, line)
+class MyCmdInterpreter(cmd.Cmd):
+    prompt = '>> '  # Set the command prompt
 
-    
-if __name__ == "__main__":
-    MyConsole().cmdloop()
+    def do_greet(self, arg):
+        """Greet the user."""
+        print(f"Hello, {arg}!")
+
+    def do_add(self, arg):
+        """Add two numbers."""
+        try:
+            numbers = [int(x) for x in arg.split()]
+            result = sum(numbers)
+            print(f"Sum: {result}")
+        except ValueError:
+            print("Invalid input. Please provide space-separated numbers.")
+
+    def do_quit(self, arg):
+        """Exit the command interpreter."""
+        print("Quitting.")
+        return True
+
+
+if __name__ == '__main__':
+    my_cmd = MyCmdInterpreter()
+    my_cmd.cmdloop("Welcome to the command interpreter!")
