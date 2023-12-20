12f1aede852ef9bb4cfb7fad840573ef25d2546e
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-12-20 21:12:28 +0100

    update base mode

--- basemodel.py
+++ basemodel.py
@@ -1,13 +1,35 @@
 #!/usr/bin/python3
 
+# I will create the Basemodel class for th airbnb clone project
+
 from datetime import datetime
 
+
 class BaseModel:
-    def __init__(self):
-        self.created_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """
+        self.created_at = datetime.now().isoformat()
+
+        print(f"created at: {self.created_at}")
+        """
+        if not hasattr(self, 'created_at'):
+            self.created_at = datetime.now().isoformat()
+            print(f"created_at: {self.created_at}")
+        
+        if args:
+            for values in args:
+                print(values)
+
+        if kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def __str__(self):
-        return "BaseModel object created at {}".format(self.created_at)
+        return "\n".join([f"{key}: {value}" for key, value in self.__dict__.items()])
 
-mod_1 = BaseModel()
-print(mod_1)
+print("==== outputs of args below ====")
+mod_1 = BaseModel("nadjib", "fares", "ali", "abdou")
+
+mod_2 = BaseModel(name="Nadjib", age=21, city="Paris", job="Software Engineer")
+print("==== outputs of kwargs below ====")
+print(mod_2)
