c65679280fc73f2c32c40c4958c0c1428f26c238
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-12-21 23:56:42 +0100

    create MyBaseClass

---
+++ MyBase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from datetime import datetime
+from uuid import uuid4
+
+class MyBaseClass:
+
+    def __init__:
+        # assign a few common default attributes to all instance of this class
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
