9b5a56e46484d901fb0c8fe7c39ce801661c4034
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-12-21 12:20:37 +0100

    update the console to handle case-insensetive input

--- console.py
+++ console.py
@@ -2,23 +2,25 @@
 
 import cmd
 
+
 class HelloWorld(cmd.Cmd):
     """Simple command processor example."""
     FRIENDS = ['Nadjib', 'Fares', 'Abdou', 'Ali']
 
     def do_greet(self, person):
-        if person and person in self.FRIENDS:
+        if person and person.lower() in [friend.lower() for friend in self.FRIENDS]:
             print(f"Hi {person}")
         elif person:
             print(f"Hello {person}")
         else:
             print("Hello")
-    
+
     def complete_greet(self, text, line, begidx, endidx):
         if not text:
             return self.FRIENDS[:]
         else:
-            return [friend for friend in self.FRIENDS if friend.startswith(text)]
+            return [friend for friend in self.FRIENDS
+                    if friend.lower().startswith(text.lower())]
 
     def help_greet(self):
         print("\n".join(["greet [person]", "Greet the named person"]))
