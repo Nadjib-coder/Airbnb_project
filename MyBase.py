f1cb527913a897d01d82d4372a40afe318ecfa80
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-12-22 15:57:09 +0100

    update

--- MyBase.py
+++ MyBase.py
@@ -14,8 +14,26 @@ class MyBaseClass:
     def save_update(self):
         self.updated_at = datetime.now()
 
-my_model = MyBaseClass()
-my_model.name = "Nadjib"
-my_model.save_update()
-print(my_model.created_at)
-print(my_model.updated_at)
+    def save_to_dict(self):
+        dict_format =  self.__dict__
+        dict_format["__class__"] = self.__class__.__name__
+        return dict_format
+
+print("=== start check save_update() method ===") 
+obj_1 = MyBaseClass()
+obj_1.name = "Nadjib"
+obj_1.save_update()
+print(obj_1.created_at)
+print(obj_1.updated_at)
+print("=== start check save_to_dict() method ===")
+my_instance_1 = MyBaseClass()
+serialized_data = my_instance_1.save_to_dict()
+my_new_instance = MyBaseClass()
+my_new_instance.__dict__.update(serialized_data)
+print("my_instance_1 info:")
+print(my_instance_1.id)
+print(my_instance_1.created_at)
+print("my_new_instance info:")
+print(my_new_instance.id)
+print(my_new_instance.created_at)
+print(my_new_instance.__class__)
