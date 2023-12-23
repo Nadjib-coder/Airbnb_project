d34d97db60d65c900c6da3e5ef2e00e28357659e
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-12-23 23:49:39 +0100

    update

--- MyBase.py
+++ MyBase.py
@@ -5,35 +5,56 @@ from uuid import uuid4
 
 class MyBaseClass:
 
-    def __init__(self):
-        # assign a few common default attributes to all instance of this class
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            del kwargs['__class__']
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    dtime_obj = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, dtime_obj)
+                else:
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save_update(self):
         self.updated_at = datetime.now()
 
     def save_to_dict(self):
-        dict_format =  self.__dict__
+        dict_format = {}
         dict_format["__class__"] = self.__class__.__name__
+
+        for key, value in self.__dict__.items():
+            if isinstance(value, datetime):
+                dict_format[key] = value.isoformat()
+            else:
+                dict_format[key] = value
+        
         return dict_format
 
-print("=== start check save_update() method ===") 
-obj_1 = MyBaseClass()
-obj_1.name = "Nadjib"
-obj_1.save_update()
-print(obj_1.created_at)
-print(obj_1.updated_at)
-print("=== start check save_to_dict() method ===")
-my_instance_1 = MyBaseClass()
-serialized_data = my_instance_1.save_to_dict()
-my_new_instance = MyBaseClass()
-my_new_instance.__dict__.update(serialized_data)
-print("my_instance_1 info:")
-print(my_instance_1.id)
-print(my_instance_1.created_at)
-print("my_new_instance info:")
-print(my_new_instance.id)
-print(my_new_instance.created_at)
-print(my_new_instance.__class__)
+    def __str__(self):
+        return f"{[self.__class__.__name__]} {self.id} {self.__dict__}"
+
+# creating a class instance and update it with a few attributes
+my_model = MyBaseClass()
+my_model.name = "Nadjib Mouhoun"
+my_model.age = 21
+print(my_model.id)
+print(my_model)
+print(my_model.created_at)
+print("\n------- Serialized to dictionary below -------\n")
+my_model_json = my_model.save_to_dict()
+print(my_model_json)
+print("\n------ JSON of <'my_model'> below ------\n")
+for key, value in my_model_json.items():
+    print("\t{}: ({}) - {}".format(key, type(value), value))
+print("\n ------ deserialized to an instance below ------\n")
+#parse in the dictionary to the class constructor for deserialization
+new_model = MyBaseClass(**my_model_json)
+print(new_model.id)
+print(new_model)
+print(new_model.created_at)
+print("\n------ the two instanceses compared below -----\n")
+print(new_model is my_model)
