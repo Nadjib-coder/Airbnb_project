a4b766adfb35942e866f9dbcf56cf97f2fafc90c
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-12-27 20:13:55 +0100

    add load method in filestorgae class

--- fileStorage.py
+++ fileStorage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from json import dump
+from json import load
 
 class FileStorage:
 
@@ -10,15 +11,30 @@ class FileStorage:
     def all(self):
         return FileStorage.__objects
 
-    def new(slef, obj):
+    def new(self, obj):
         keyFormat = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects[keyFormat] = obj
 
     def save(self):
         dictObj = {}
-        
-        for key, value in FileStorage.__objects.items():
+        for key, value in fileStorage.__objetcs.items():
             dictObj[key] = value.save_to_dict()
-        
-        with open(FileStorage.__filePath, "w", encoding="utf-8") as jsonFile:
+        with open(FileStorage.__filePath, "W", encoding="utf-8") as jsonFile:
             dump(dictObj, jsonFile)
+
+    def reload(self):
+
+        from MyPackage.myFileName import MyBase
+        definedClasses = {'MyBase': MyBase}
+
+        try:
+            with open(FileStorage.__objetcs, encoding="utf-8") as jsonStr:
+                deserialized = load(jsonFile)
+
+                for objValue in deserialized.values():
+                    clsName = objValue["__class__"]
+                    clsObj = definedClasses[clsName]
+                    self.new(clsObj(**objValue))
+
+        except FileNotFoundError:
+            pass
